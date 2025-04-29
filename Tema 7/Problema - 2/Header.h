5401ce436cef136a71036b555e99ef1ec4546262
Isari Andrew <115490605+itsmenode@users.noreply.github.com>
2025-04-29 18:39:00 +0300

    Header.h

--- Tema 7/Problema - 2/Header.h
+++ Tema 7/Problema - 2/Header.h
@@ -1,233 +1,303 @@
+#pragma once
+
 #include <iostream>
 
 template <typename T>
-class Tree {
+class arbore {
 private:
-    struct Node {
-        T value;
-        Node* parent;
-        Node** children;  
-        int capacity;
-        int size;
+	class nod {
+	public:
+		T valoare;
+		nod* parinte;
+		nod* copil;
+		nod* frate;
 
-        Node(const T& val, Node* p = nullptr) : value(val), parent(p), capacity(10), size(0) {
-            children = new Node * [capacity];
-            for (int i = 0; i < capacity; i++) {
-                children[i] = nullptr;
-            }
-        }
+		nod(const T& val, nod* p = nullptr) : valoare(val), parinte(p), copil(nullptr), frate(nullptr) {}
+	};
 
-        ~Node() {
-            delete[] children;
-        }
-
-        void resize() {
-            int new_capacity = capacity * 2;
-            Node** new_children = new Node * [new_capacity];
-
-            for (int i = 0; i < size; i++) {
-                new_children[i] = children[i];
-            }
-
-            for (int i = size; i < new_capacity; i++) {
-                new_children[i] = nullptr;
-            }
-
-            delete[] children;
-            children = new_children;
-            capacity = new_capacity;
-        }
-
-        void add_child(Node* child) {
-            if (size >= capacity) {
-                resize();
-            }
-            children[size++] = child;
-        }
-
-        void insert_child(Node* child, int index) {
-            if (index < 0 || index > size) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            if (size >= capacity) {
-                resize();
-            }
-
-            for (int i = size; i > index; i--) {
-                children[i] = children[i - 1];
-            }
-
-            children[index] = child;
-            size++;
-        }
-
-        void remove_child(int index) {
-            if (index < 0 || index >= size) {
-                throw std::out_of_range("Index out of range");
-            }
-
-            for (int i = index; i < size - 1; i++) {
-                children[i] = children[i + 1];
-            }
-
-            children[size - 1] = nullptr;
-            size--;
-        }
-    };
-
-    Node* root;
-
-    void delete_subtree(Node* node) {
-        if (!node) return;
-
-        for (int i = 0; i < node->size; i++) {
-            delete_subtree(node->children[i]);
-        }
-
-        delete node;
-    }
-
-    Node* find_node(Node* node, const T& value, bool (*compare)(const T&, const T&)) {
-        if (!node) return nullptr;
-
-        if (compare(node->value, value)) {
-            return node;
-        }
-        for (int i = 0; i < node->size; i++) {
-            Node* found = find_node(node->children[i], value, compare);
-            if (found) return found;
-        }
-
-        return nullptr;
-    }
-
-    int count_nodes(Node* node) const {
-        if (!node) return 0;
-
-        int count = 1;
-        for (int i = 0; i < node->size; i++) {
-            count += count_nodes(node->children[i]);
-        }
-
-        return count;
-    }
-
-    void sort_children(Node* node, bool (*compare)(const T&, const T&)) {
-        if (!node || node->size <= 1) return;
-
-        for (int i = 0; i < node->size - 1; i++) {
-            for (int j = 0; j < node->size - i - 1; j++) {
-                if (compare) {
-                    if (compare(node->children[j + 1]->value, node->children[j]->value)) {
-                        Node* temp = node->children[j];
-                        node->children[j] = node->children[j + 1];
-                        node->children[j + 1] = temp;
-                    }
-                }
-                else {
-                    if (node->children[j + 1]->value < node->children[j]->value) {
-                        Node* temp = node->children[j];
-                        node->children[j] = node->children[j + 1];
-                        node->children[j + 1] = temp;
-                    }
-                }
-            }
-        }
-    }
+	nod* radacina;
 
 public:
-    Tree() : root(nullptr) {}
+	arbore();
+	~arbore();
 
-    ~Tree() {
-        delete_node(nullptr);
-    }
+	nod* add_node(const T& valoare, nod* parinte);
+	nod* get_node(nod* parinte);
+	void delete_node(nod* node);
+	nod* find(const T& valoare, bool (*comparator)(const T&, const T&));
+	nod* insert(const T& valoare, nod* parinte, int i);
+	void sort(nod* node, bool (*comparator)(const T&, const T&));
+	int count(nod* node);
 
-    Node* add_node(const T& value, Node* parent = nullptr) {
-        Node* new_node = new Node(value, parent);
+};
 
-        if (!parent) {
-            if (root) {
-                delete new_node;
-                throw std::runtime_error("Root already exists");
+
+template <typename T>
+arbore<T>::arbore() {
+	radacina = nullptr;
+}
+
+template <typename T>
+arbore<T>::~arbore() {
+	if (radacina == nullptr) return;
+	nod* actual = radacina;
+	nod* temp;
+
+	while (actual != nullptr) {
+		if (actual->copil != nullptr) {
+			temp = actual;
+			actual = actual->copil;
+			temp->copil = nullptr;
+		}
+		else {
+			temp = actual;
+			if (actual->frate != nullptr) actual = actual->frate;
+			else actual = actual->parinte;
+			delete temp;
+		}
+	}
+
+	radacina = nullptr;
+}
+
+template <typename T>
+arbore<T>::nod* arbore<T>::add_node(const T& valoare, nod* parinte) {
+	nod* newnod = new nod(valoare, parinte);
+
+	if (parinte == nullptr) {
+		if (radacina != nullptr) {
+			delete_node(radacina);
+		}
+		radacine = newnod;
+	}
+
+	else {
+		if (parinte->copil == nullptr) parinte->copil = newnod;
+		else {
+			nod* fratele = parinte->frate;
+			while (fratele->frate != nullptr) {
+				fratele = fratele->frate;
+			}
+			fratele->frate = newnod;
+		}
+	}
+	return newnod;
+}
+
+template <typename T>
+arbore<T>::nod* arbore<T>::get_node(nod* parinte) {
+	if (parinte == nullptr) return radacina;
+	return parinte->copil;
+}
+
+template <typename T>
+void arbore<T>::delete_node(nod* node) {
+    if (node == nullptr) return;
+
+    if (node == radacina) {
+        nod* actual = radacina;
+        nod* temp;
+
+        while (actual != nullptr) {
+            if (actual->copil != nullptr) {
+                temp = actual;
+                actual = actual->copil;
+                temp->copil = nullptr;
             }
-            root = new_node;
-        }
-        else parent->add_child(new_node);
+            else {
+                temp = actual;
 
-        return new_node;
+                if (actual->frate != nullptr) {
+                    actual = actual->frate;
+                }
+                else {
+                    actual = actual->parinte;
+                }
+
+                delete temp;
+            }
+        }
+
+        radacina = nullptr;
+        return;
     }
 
-    Node* get_node(Node* parent = nullptr) const {
-        if (!parent) {
-            return root;
+    nod* parinte = node->parinte;
+    if (parinte) {
+        if (parinte->copil == node) {
+            parinte->copil = node->frate;
         }
-        throw std::invalid_argument("Invalid parent node");
-        return nullptr;
-    }
-
-    void delete_node(Node* node) {
-        if (!node) {
-            delete_subtree(root);
-            root = nullptr;
-            return;
-        }
-
-        Node* parent = node->parent;
-        if (!parent) root = nullptr;
         else {
-            int index = -1;
-            for (int i = 0; i < parent->size; i++) {
-                if (parent->children[i] == node) {
-                    index = i;
+            nod* sibling = parinte->copil;
+            while (sibling && sibling->frate != node) {
+                sibling = sibling->frate;
+            }
+            if (sibling) {
+                sibling->frate = node->frate;
+            }
+        }
+    }
+
+    nod* actual = node;
+    nod* temp;
+
+    while (actual != nullptr) {
+        if (actual->copil != nullptr) {
+            temp = actual;
+            actual = actual->copil;
+            temp->copil = nullptr;
+        }
+        else {
+            temp = actual;
+
+            if (actual->frate != nullptr) {
+                actual = actual->frate;
+            }
+            else {
+                actual = actual->parinte;
+                if (actual == parinte) {
+                    actual = nullptr;
+                }
+            }
+
+            delete temp;
+        }
+    }
+}
+
+template <typename T>
+arbore<T>::nod* arbore<T>::find(const T& valoare, bool (*comparator)(const T&, const T&)) {
+    if (radacina == nullptr) return nullptr;
+    nod* actual = radacina;
+    while (actual != nullptr) {
+        if (comparator(actual->valoare, valoare)) return actual;
+
+        if(actual->copil != nullptr) actual = actual->copil;
+        else {
+            while (actual != nullptr && actual->frate == nullptr) actual = actual->parinte;
+            if (actual != nullptr) actual = actual->frate;
+        }
+    }
+
+    return nullptr;
+}
+
+template <typename T>
+arbore<T>::nod* arbore<T>::insert(const T& valoare, nod* parinte, int i) {
+    if (parinte == nullptr) return add_node(valoare, nullptr);
+
+    nod* newNode = new nod(valoare, parinte);
+    if (i == 0) {
+        newNode->frate = parinte->copil;
+        parinte->copil = newNode;
+    }
+    else {
+        nod* anterior = parinte->copil;
+        int j = 0;
+
+        while (anterior && j < i - 1) {
+            anterior = anterior->frate;
+            j++;
+        }
+
+        if (anterior) {
+            newNode->frate = anterior->frate;
+            anterior->frate = newNode;
+        }
+        else return add_node(valoare, parinte);
+
+    }
+
+    return newNode;
+}
+
+template <typename T>
+void arbore<T>::sort(nod* node, bool (*comparator)(const T&, const T&)) {
+    if (node == nullptr) {
+        node = radacina;
+        if (node == nullptr) return;
+    }
+
+    if (node->copil != nullptr) {
+        bool ok;
+        nod* final = nullptr;
+
+        do {
+            ok = false;
+            nod* actual = nod->copil;
+            nod* anterior = nullptr;
+
+            while (actual->frate != final) {
+                nod* urmator = actual->frate;
+
+                bool ok2;
+                if (comparator != nullptr) {
+                    ok2 = comparator(urmator->valoare, actual->valoare);
+                }
+                else {
+                    ok2 = urmator->valoare < actual->valoare;
+                }
+
+                if (ok2) {
+                    if (anterior) anterior->frate = urmator;
+                    else node->copil = urmator;
+
+                    actual->frate = urmator->frate;
+                    urmator->frate = actual;
+
+                    anterior = urmator;
+                    ok = true;
+                }
+                else {
+                    anterior = actual;
+                    actual = actual->frate;
+                }
+            }
+            final = actual;
+        } while (ok);
+
+        nod* child = node->copil;
+        while (child) {
+            sort(child, comparator);
+            child = child->frate;
+        }
+    }
+}
+
+template <typename T>
+int arbore<T>::count(nod* node) {
+    if (node == nullptr) {
+        if (radacina == nullptr) return 0;
+        node = radacina;
+    }
+
+    int cnt = 0;
+
+    Node* actual = node;
+    Node* anterior = nullptr;
+
+    while (actual != nullptr) {
+        cnt++;
+
+        if (actual->copil != nullptr) {
+            actual = actual->copil;
+        }
+        else {
+            while (actual != nullptr && (actual->frate == nullptr || actual == node)) {
+                anterior = actual;
+                actual = actual->parinte;
+
+                if (actual == node->parinte) {
+                    actual = nullptr;
                     break;
                 }
             }
 
-            if (index >= 0) {
-                parent->remove_child(index);
+            if (actual != nullptr && actual != node->parinte) {
+                actual = actual->frate;
             }
         }
-
-        delete_subtree(node);
     }
 
-    Node* find(const T& value, bool (*compare)(const T&, const T&)) {
-        return find_node(root, value, compare);
-    }
-
-    Node* insert(const T& value, Node* parent, int index) {
-        if (!parent && !root) {
-            return add_node(value);
-        }
-        else if (!parent) {
-            throw std::invalid_argument("Cannot insert at an index with nullptr parent when root exists");
-        }
-
-        Node* new_node = new Node(value, parent);
-        parent->insert_child(new_node, index);
-        return new_node;
-    }
-
-    void sort(Node* node = nullptr, bool (*compare)(const T&, const T&) = nullptr) {
-        if (!node) {
-            node = root;
-        }
-
-        if (!node) return;
-
-        sort_children(node, compare);
-
-        for (int i = 0; i < node->size; i++) {
-            sort(node->children[i], compare);
-        }
-    }
-
-    int count(Node* node = nullptr) const {
-        if (!node) {
-            node = root;
-        }
-
-        return count_nodes(node);
-    }
-};
+    return cnt;
+}
